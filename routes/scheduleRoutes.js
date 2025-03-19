6f50b2bc7a2eef937b97fe5b9f6b46239fdd0be3
kdrm <kdonram@gmail.com>
2025-03-19 16:42:53 +0530

    Changes in student schedule router.

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getStudentSchedule, addEventToSchedule, updateEventInSchedule, deleteEventFromSchedule } from '../controllers/scheduleController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
+import { roleMiddleware } from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', authMiddleware, getStudentSchedule);
-router.post('/events', authMiddleware, addEventToSchedule);
-router.put('/events/:eventId', authMiddleware, updateEventInSchedule);
-router.delete('/events/:eventId', authMiddleware, deleteEventFromSchedule);
+router.get('/', authMiddleware, roleMiddleware(['student']), getStudentSchedule);
+router.post('/events', authMiddleware, roleMiddleware(['student']), addEventToSchedule);
+router.put('/events/:eventId', authMiddleware, roleMiddleware(['student']), updateEventInSchedule);
+router.delete('/events/:eventId', authMiddleware, roleMiddleware(['student']), deleteEventFromSchedule);
 
 export default router;

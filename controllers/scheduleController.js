8394c4d8ae7b936dee2b3709e79ed4f4533f3f54
Kasun Madhusanka Jayasinghe <officialkasun@officialkasun.local>
2025-03-20 19:11:07 +0530

    Updated Schedule Page

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -23,6 +23,32 @@ export const addEventToSchedule = async (req, res) => {
     }
 };
 
+
+// Add Event to Schedule
+export const addEventToScheduleByAdmin = async (req, res) => {
+    const { studentId, events } = req.body;
+
+    const { title, description, date, location, type } = events;
+  
+    try {
+        let schedule = await Schedule.findOne({ studentId });
+    
+        if (!schedule) {
+            schedule = await Schedule.create({ studentId, events: [] });
+        }
+        
+        
+    
+        schedule.events.push({ title, description, date, location, type });
+        await schedule.save();
+  
+      res.status(201).json(schedule);
+    } catch (error) {
+        console.error('Error adding event to schedule:', error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
 // Get student schedule
 export const getStudentSchedule = async (req, res) => {
     const studentId = req.user._id;
@@ -36,6 +62,22 @@ export const getStudentSchedule = async (req, res) => {
         res.status(200).json(schedule);
     } catch (error) {
         console.error('Error fetching schedule:', error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+
+// Get all student schedules for admin
+export const getStudentScheduleByAdmin = async (req, res) => {
+    try {
+        const schedules = await Schedule.find().populate('studentId', 'name email');
+        if (!schedules || schedules.length === 0) {
+            return res.status(404).json({ message: 'No schedules found' });
+        }
+    
+        res.status(200).json(schedules);
+    } catch (error) {
+        console.error('Error fetching schedules:', error);
         res.status(500).json({ message: 'Something went wrong' });
     }
 };
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { getStudentSchedule, addEventToSchedule, updateEventInSchedule, deleteEventFromSchedule, addEventToStudentSchedule, getStudentScheduleById } from '../controllers/scheduleController.js';
+import { getStudentSchedule, addEventToSchedule, updateEventInSchedule, deleteEventFromSchedule, addEventToStudentSchedule, getStudentScheduleById, addEventToScheduleByAdmin, getStudentScheduleByAdmin } from '../controllers/scheduleController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import { roleMiddleware } from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', authMiddleware, roleMiddleware(['student' , 'admin']), getStudentSchedule);
-router.post('/events', authMiddleware, roleMiddleware(['student' , 'admin']), addEventToSchedule);
+router.get('/', authMiddleware, roleMiddleware(['student']), getStudentSchedule);
+
+router.get('/event', authMiddleware, roleMiddleware(['student' , 'admin']), getStudentScheduleByAdmin);
+router.post('/', authMiddleware, roleMiddleware(['student' , 'admin']), addEventToSchedule);
+router.post('/event', authMiddleware, roleMiddleware(['admin']), addEventToScheduleByAdmin);
 router.put('/events/:eventId', authMiddleware, roleMiddleware(['student' , 'admin']), updateEventInSchedule);
 router.delete('/events/:eventId', authMiddleware, roleMiddleware(['student' , 'admin']), deleteEventFromSchedule);
 

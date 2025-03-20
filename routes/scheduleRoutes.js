c812fffa72981f6aa9404caaff7897b011289083
kdrm <kdonram@gmail.com>
2025-03-20 18:02:05 +0530

    error in git

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -5,18 +5,12 @@ import { roleMiddleware } from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
-<<<<<<< HEAD
+
 router.get('/', authMiddleware, roleMiddleware(['student']), getStudentSchedule);
 router.post('/events', authMiddleware, roleMiddleware(['student']), addEventToSchedule);
 router.put('/events/:eventId', authMiddleware, roleMiddleware(['student']), updateEventInSchedule);
 router.delete('/events/:eventId', authMiddleware, roleMiddleware(['student']), deleteEventFromSchedule);
 router.post('/:stuId', authMiddleware, roleMiddleware(['admin', 'lecturer']), addEventToStudentSchedule);
 router.get('/:stuId', authMiddleware, roleMiddleware(['admin', 'lecturer']), getStudentScheduleById);
-=======
-router.get('/', authMiddleware, roleMiddleware(['student' , 'admin']), getStudentSchedule);
-router.post('/events', authMiddleware, roleMiddleware(['student' , 'admin']), addEventToSchedule);
-router.put('/events/:eventId', authMiddleware, roleMiddleware(['student' , 'admin']), updateEventInSchedule);
-router.delete('/events/:eventId', authMiddleware, roleMiddleware(['student' , 'admin']), deleteEventFromSchedule);
->>>>>>> f46594cfe97c5e4ca9461bb837b9a8d71e927a3f
 
 export default router;
